c4525cd23b20c7e1d794094e8daef415304abd6c
吴子豪 <vortez.wohl@gmail.com>
2025-06-17 16:30:43 +0800

    feat: 实现线程池 submit 方法

--- vortezwohl/concurrent/thread_pool.py
+++ vortezwohl/concurrent/thread_pool.py
@@ -1,7 +1,9 @@
-from concurrent.futures import ThreadPoolExecutor, as_completed
+import traceback
+from concurrent.futures import ThreadPoolExecutor, Future, as_completed
+from threading import Lock
 from types import NoneType
 
-from typing_extensions import Iterable, Any
+from typing_extensions import Iterable, Any, Callable, Dict, Tuple
 
 import psutil
 
@@ -12,6 +14,8 @@ class ThreadPool(object):
             max_workers = psutil.cpu_count(logical=True) + 1
         self._max_workers = max_workers
         self._thread_pool_executor = ThreadPoolExecutor(max_workers=self._max_workers)
+        self._futures = list()
+        self._futures_lock = Lock()
 
     def __enter__(self):
         return self
@@ -19,30 +23,52 @@ class ThreadPool(object):
     def __exit__(self, exc_type, exc_val, exc_tb):
         return self.shutdown(cancel_futures=False)
 
-    def gather(self, jobs: Iterable[callable], arguments: list[dict] | list[tuple] | list[NoneType] | None = None) -> Any:
+    def submit(self, job: Callable, *args, **kwargs) -> Future:
+        _future = None
+        if len(args) > 0:
+            _future = self._thread_pool_executor.submit(lambda param: (job, param, job(*param)), args)
+        elif len(kwargs) > 0:
+            _future = self._thread_pool_executor.submit(lambda param: (job, param, job(**param)), kwargs)
+        else:
+            _future = self._thread_pool_executor.submit(lambda: (job, None, job()))
+        with self._futures_lock:
+            self._futures.append(_future)
+        return _future
+
+    def gather(self, jobs: Iterable[Callable], arguments: list[Dict] | list[Tuple] | list[NoneType] | None = None) \
+            -> tuple[Callable, Any, Any] | Exception:
         futures = []
         for i, job in enumerate(jobs):
             if arguments is None or arguments[i] is None:
-                futures.append(self._thread_pool_executor.submit(job))
+                futures.append(self._thread_pool_executor.submit(lambda: (job, None, job())))
                 continue
             if isinstance(arguments[i], dict):
-                futures.append(self._thread_pool_executor.submit(lambda param: job(**param), arguments[i]))
+                futures.append(self._thread_pool_executor.submit(lambda param: (job, param, job(**param)), arguments[i]))
             elif isinstance(arguments[i], Iterable):
-                futures.append(self._thread_pool_executor.submit(lambda param: job(*param), arguments[i]))
+                futures.append(self._thread_pool_executor.submit(lambda param: (job, param, job(*param)), arguments[i]))
             else:
-                futures.append(self._thread_pool_executor.submit(job, arguments[i]))
+                futures.append(self._thread_pool_executor.submit(lambda param: (job, param, job(param)), arguments[i]))
         for future in as_completed(futures):
             try:
                 yield future.result()
             except Exception as __e:
-                yield __e
+                yield __e, traceback.format_exc()
 
     def shutdown(self, cancel_futures: bool = True):
         self._thread_pool_executor.shutdown(wait=True, cancel_futures=cancel_futures)
         return self
 
     @property
-    def thread_pool_executor(self):
+    def next_result(self) -> Any:
+        for f in as_completed(self._futures):
+            try:
+                yield f.result()
+            finally:
+                with self._futures_lock:
+                    self._futures.remove(f)
+
+    @property
+    def thread_pool_executor(self) -> ThreadPoolExecutor:
         return self._thread_pool_executor
 
     @property
