7bf269ae3dff3791e191cac504521d43330fc701
吴子豪 <vortez.wohl@gmail.com>
2025-07-24 13:37:46 +0800

    feat: 优化随机种子生成器

--- vortezwohl/random/seed_generator.py
+++ vortezwohl/random/seed_generator.py
@@ -1,16 +1,20 @@
+import os
 import threading
 import random
 
-SEED_LOCK = threading.Lock()
-SEED_INCREMENT = 0
-SEED_MULTIPLIER = 1
-MOD = int(1e16)
+SEED_LOCK = threading.RLock()
+SEED_INCREMENT = None
+SEED_MULTIPLIER = None
+MOD = int(1e64)
 
 
 def next_seed() -> int:
     global SEED_INCREMENT, SEED_MULTIPLIER
-    with SEED_LOCK:
-        SEED_INCREMENT = ((SEED_INCREMENT + 1) * SEED_MULTIPLIER * max((SEED_INCREMENT // MOD), 1)) % MOD
-        SEED_MULTIPLIER += 1
-        random.seed(SEED_INCREMENT)
-        return SEED_INCREMENT
+    SEED_INCREMENT = int(os.getenv('INITIAL_SEED', 0)) if SEED_INCREMENT is None else SEED_INCREMENT
+    SEED_MULTIPLIER = int(os.getenv('SEED_MULTIPLIER', 1)) if SEED_MULTIPLIER is None else SEED_MULTIPLIER
+    SEED_LOCK.acquire(blocking=True)
+    SEED_INCREMENT = ((SEED_INCREMENT + 1) * SEED_MULTIPLIER * max((SEED_INCREMENT // MOD), 1)) % MOD
+    SEED_MULTIPLIER += 1
+    SEED_LOCK.release()
+    random.seed(SEED_INCREMENT)
+    return SEED_INCREMENT
