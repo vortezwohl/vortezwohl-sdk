78c7390151a48bebab59eaa2d12d440ebf943640
吴子豪 <vortez.wohl@gmail.com>
2025-07-24 14:30:31 +0800

    feat: 实现 cache 的魔法方法

--- vortezwohl/cache/base_cache.py
+++ vortezwohl/cache/base_cache.py
@@ -31,3 +31,44 @@ class BaseCache:
                 del self._cache[key]
                 return value
         return
+
+    def __contains__(self, key: Any) -> bool:
+        contains = False
+        with self._cache_lock:
+            contains = (key in self._cache.keys())
+        return contains
+
+    def __getitem__(self, key: Any) -> Any:
+        return self.read(key=key)
+
+    def __setitem__(self, key: Any, value: Any) -> Any | None:
+        return self.write(key=key, value=value)
+
+    def __delitem__(self, key: Any) -> Any | None:
+        return self.delete(key=key)
+
+    def __str__(self) -> str:
+        _max_len = 8
+        for key, value in self._cache.items():
+            _max_len = max(_max_len, len(str(key)))
+            _max_len = max(_max_len, len(str(value)))
+        _padding = 2
+        _max_len += _padding
+        line_spliter = f'\n{"+" + "=" * (_max_len * 2 + 1) + "+"}\n'
+        header = f'{self.__class__.__name__}(capacity={len(self._cache.keys())})'
+        return (f'{header:^{_max_len * 2 + 2}}{line_spliter}'
+                + f'|{"Key":^{_max_len}}|{"Value":^{_max_len}}|{line_spliter}'
+                + line_spliter.join([f'|{str(k):^{_max_len}}|{str(v):^{_max_len}}|' for k, v in self._cache.items()])
+                + (line_spliter if len(self) else ''))
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def __len__(self) -> int:
+        return len(self._cache.keys())
+
+    def __eq__(self, other) -> bool:
+        return str(self) == str(other)
+
+    def __call__(self, key: Any) -> Any:
+        return self.read(key=key)
