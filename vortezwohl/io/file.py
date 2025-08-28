06a311d6dd99c798a9dd531ae655d20882cf4299
吴子豪 <vortez.wohl@gmail.com>
2025-08-28 17:30:32 +0800

    feat: 实现文件和路径大小计算

--- vortezwohl/io/file.py
+++ vortezwohl/io/file.py
@@ -32,3 +32,26 @@ def get_files(path: str) -> list:
         return [os.path.join(path, entry) for entry in entries if os.path.isfile(os.path.join(path, entry))]
     else:
         return []
+
+
+def size_of(path: str) -> int:  # bytes
+    def size_of_dir(dir_path: str) -> int:
+        total_size = 0
+        if not os.path.exists(dir_path) or not os.path.isdir(dir_path):
+            return 0
+        for item in os.listdir(dir_path):
+            item_path = os.path.join(dir_path, item)
+            if os.path.isfile(item_path):
+                total_size += os.path.getsize(item_path)
+            elif os.path.isdir(item_path):
+                total_size += size_of_dir(item_path)
+        return total_size
+
+    if not os.path.exists(path):
+        return 0
+    if os.path.isfile(path):
+        return os.path.getsize(path)
+    elif os.path.isdir(path):
+        return size_of_dir(path)
+    else:
+        raise TypeError(f'The path "{path}" is not a file or directory.')
