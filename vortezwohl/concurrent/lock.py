c3c054e51394f30464f4025e4cc550dfa1b3d579
吴子豪 <vortez.wohl@gmail.com>
2025-08-28 17:00:05 +0800

    feat: lock_on 装饰器支持信号量和 acquire 传参

--- vortezwohl/concurrent/lock.py
+++ vortezwohl/concurrent/lock.py
@@ -2,12 +2,17 @@ import threading
 from typing_extensions import Callable, Union
 
 
-def lock_on(lock: Union[threading.Lock, threading.RLock, threading.Semaphore]):
+def lock_on(lock: Union[threading.Lock, threading.RLock, threading.Semaphore], **kwargs):
     def decorator(func: Callable):
-        def wrapper(*args, **kwargs):
-            lock.acquire(blocking=True)
-            _ret = func(*args, **kwargs)
-            lock.release()
+        def wrapper(*_args, **_kwargs):
+            if 'blocking' not in kwargs or kwargs.get('timeout', None) is not None:
+                kwargs['blocking'] = True
+            lock.acquire(**kwargs)
+            _ret = func(*_args, **_kwargs)
+            if isinstance(lock, threading.Semaphore):
+                lock.release(n=1)
+            else:
+                lock.release()
             return _ret
         return wrapper
     return decorator
